5936b627f113ac1405634292e2886ceb72353985
saiful <saiful0687@gmail.com>
2024-01-20 13:34:58 -0500

    tech store list

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:practice_app/different_screen/listtile_demo.dart';
 import 'package:practice_app/different_screen/new_column.dart';
 import 'package:practice_app/different_screen/row_page.dart';
 import 'package:practice_app/sum_app/SumApp.dart';
+import 'package:practice_app/tech_store_app/splash_screen.dart';
 import 'package:practice_app/tech_store_app/techStoreApp.dart';
 
 import 'different_screen/login_design.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TechStore(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/tech_store_app/techStoreApp.dart
+++ lib/tech_store_app/techStoreApp.dart
@@ -1,4 +1,7 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+import 'package:practice_app/tech_store_app/dashboard_page.dart';
 
 class TechStore extends StatefulWidget {
   const TechStore({Key? key}) : super(key: key);
@@ -8,8 +11,80 @@ class TechStore extends StatefulWidget {
 }
 
 class _TechStoreState extends State<TechStore> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  GlobalKey<FormState> _keyTech = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return Scaffold(
+
+      body: Form(
+        key: _keyTech,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  hintText: "Email",
+                ),
+                validator: (String? value) {
+                  String v = value ?? "";
+                  if (v.isEmpty) {
+                    return "Input Email";
+                  }
+                  //(String ? text){
+                  // text!.isEmpty}
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                    hintText: "Number",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                validator: (String? value) {
+                  String v = value ?? "";
+                  if (v.isEmpty) {
+                    return "Pnone Number";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (_keyTech.currentState!.validate()) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomeScreen();
+                          }));
+                        }
+                      },
+                      child: const Text("Next")))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
